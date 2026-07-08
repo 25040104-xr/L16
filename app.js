e793de2d4de57b3a0977f353cd5d51378c999151
yourname <youremail@myrp.edu.sg>
2026-07-08 14:24:29 +0800

    add route for  edit and delete product

--- app.js
+++ app.js
@@ -64,6 +64,7 @@ app.get('/product/:id', (req, res) => {
 app.get('/addProduct', (req, res) => {
   res.render('addProduct'); 
 });
+
 app.post('/addProduct', (req, res) => {
   // Extract product data from the request body
   const { name, quantity, price, image } = req.body;
@@ -74,6 +75,62 @@ app.post('/addProduct', (req, res) => {
       // Handle any error that occurs during the database operation
       console.error("Error adding product:", error);
       res.send('Error adding product');
+    } else {
+      // Send a success response
+      res.redirect('/');
+    }
+  });
+});
+
+app.get('/editProduct/:id', (req,res) => {
+  const productId = req.params.id;
+  const sql = 'SELECT * FROM products WHERE productId = ?';
+  // Fetch data from MySQL based on the product ID
+  connection.query( sql , [productId], (error, results) => {
+    if (error) {
+      console.error('Database query error:', error.message); 
+      return res.send('Error retrieving product by ID'); 
+    }
+    // Check if any product with the given ID was found
+    if (results.length > 0) {
+      // Render HTML page with the product data
+      res.render('editProduct', { product: results[0] });
+    } else {
+      // If no product with the given ID was found, render a 404 page or handle it accordingly
+      res.send('Product not found');
+    }
+  });
+});
+
+app.post('/editProduct/:id', (req, res) => {
+  const productId = req.params.id;
+  // Extract product data from the request body
+  const { name, quantity, price } = req.body;
+  if (req.file) { //if new image is uploaded
+    image = req.file.filename; // set image to be new image filename
+  } 
+  const sql = 'UPDATE products SET productName = ? , quantity = ?, price = ? WHERE productId = ?';
+  // Insert the new product into the database
+  connection.query( sql , [name, quantity, price, productId], (error, results) => {
+    if (error) {
+      // Handle any error that occurs during the database operation
+      console.error("Error updating product:", error);
+      res.send('Error updating product');
+    } else {
+      // Send a success response
+      res.redirect('/');
+    }
+  });
+});
+
+app.get('/deleteProduct/:id', (req, res) => {
+  const productId = req.params.id;
+  const sql = 'DELETE FROM products WHERE productId = ?';
+  connection.query( sql , [productId], (error, results) => {
+    if (error) {
+      // Handle any error that occurs during the database operation
+      console.error("Error deleting product:", error);
+      res.send('Error deleting product');
     } else {
       // Send a success response
       res.redirect('/');
